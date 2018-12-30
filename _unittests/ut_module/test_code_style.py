1fcf6d7c2ae8805446fdd6d48ed4fdbc5d204b76
xavier dupré <xavier.dupre@gmail.com>
2018-12-30 15:55:11 +0100

    Update test_code_style.py

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -29,7 +29,7 @@ class TestCodeStyle(ExtTestCase):
         src_ = os.path.normpath(os.path.join(thi, "..", "..", "src"))
         check_pep8(src_, fLOG=fLOG,
                    pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
-                                  'W0703', 'W0123', 'C0302', 'W0622'),
+                                  'W0703', 'W0123', 'C0302', 'W0622', 'W0107'),
                    skip=["Attribute 'session_params' defined outside __init__",
                          "Consider iterating the dictionary directly instead of calling .keys()",
                          "azure_transfer_api.py:20: W0231",
@@ -42,7 +42,7 @@ class TestCodeStyle(ExtTestCase):
         test = os.path.normpath(os.path.join(thi, "..", ))
         check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*",
                    pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
-                                  'C0111', 'W0511', 'W0622', 'R1710', 'E1133'),
+                                  'C0111', 'W0511', 'W0622', 'R1710', 'E1133', 'W0107'),
                    skip=["src' imported but unused",
                          "skip_' imported but unused",
                          "skip__' imported but unused",
